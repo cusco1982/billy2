1f10b70bad7a407aff3526cefa1e122a4b563b62
Julian <cusco2891@gmail.com>
2020-02-26 23:45:20 -0500

    need to pass more data from registration thru

--- client/src/pages/Tenant.js
+++ client/src/pages/Tenant.js
@@ -117,6 +117,7 @@ class Tenant extends Component {
       </div>
 
 
+
           <div className="row justify-content-center">
             <div className="col-md-5 col3 animated fadeInUp slow delay">
               <div className="jumbotron tent2">
